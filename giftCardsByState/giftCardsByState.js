6d464db3310c04a4fc94a51627ddf36450e3bba9
btidwell <btidwell@users.noreply.github.com>
2015-10-06 14:49:46 -0700

    add giftCardByState, projectsByState, and simple api_tester

---
+++ giftCardsByState/giftCardsByState.js
@@ -0,0 +1,29 @@
+require('dotenv').load();
+var pg = require('pg');
+var conString = "postgres://" + process.env.DB_USER + ":" + process.env.DB_PASS +"@donors-choose.ct8ks5pp86im.us-west-2.redshift.amazonaws.com:5439/donorschoose";
+
+exports.handler = function(event, context) {
+    var state = (event.state === undefined ? '' : event.state);
+    if(state === '') {
+        context.fail('State not provided');
+    }
+    else {
+        pg.connect(conString, function(err, client, done) {
+            if(err) {
+                context.fail(err);
+                return console.error('error fetching client from pool', err);
+            }
+            client.query("SELECT COUNT(*) AS total_giftcards FROM donorschoose_giftcards WHERE recipient_state = $1", [state], function(err, result) {
+                done();
+
+                if(err) {
+                    return console.error('error running query', err);
+                }
+                client.end();
+                console.log(result.rows[0]);
+                context.succeed(result.rows[0]);
+
+            });
+        });
+    }
+};
